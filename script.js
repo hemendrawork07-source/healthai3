8bfcf59083d746e6522b26511f7a9a3d4861d1be
hemendrawork07 <hemendrawork.07@gmail.com>
2026-08-14 14:08:30 +0530

    Prepare HealthAI for Azure deployment

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 (() => {
     "use strict";
   
-    const API_BASE = "https://healthai-4-k0tk.onrender.com/";
+    const API_BASE = window.API_BASE || "https://healthai-4-k0tk.onrender.com";
   
     const form = document.getElementById("predict-form");
     const submitBtn = document.getElementById("submit-btn");
